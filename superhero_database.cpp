b05cf8f83db445380d1f79eb1676a87b93cc6e8b
DevanM117 <devanmarion8@gmail.com>
2025-04-21 02:14:07 +0000

    Added Helper function

--- superhero_database.cpp
+++ superhero_database.cpp
@@ -12,3 +12,26 @@
 using namespace std;
 
 const int SIZE = 10;
+
+// Parallel Arrays
+string names[SIZE] = {
+    "Squirrel Girl", "Arm-Fall-Off-Boy", "The Tick", "Moon Knight", "Matter-Eater Lad",
+    "Ragman", "Doorman", "Zeitgeist", "Ambush Bug", "Hellcow"
+};
+
+string powers[SIZE] = {
+    "Control Squirrels", "Detachable Limbs", "Super Strength", "Multiple Personalities", "Eat Anything",
+    "Absorbs Evil Souls", "Teleportation via Doors", "Acid Vomit", "Teleportation", "Vampiric Cow Abilities"
+};
+
+string weaknesses[SIZE] = {
+    "Time Limits", "Heavy Objects", "Poor Judgment", "Mental Stability", "Stomach Capacity",
+    "Moral Compass", "Must Use Doors", "Digestive Issues", "Too Random", "Sunlight"
+};
+
+// Multidimensional Array (2D array for year and universe)
+string metadata[SIZE][2] = {
+    {"1992", "Marvel"}, {"1989", "DC"}, {"1986", "Other"}, {"1975", "Marvel"}, {"1962", "DC"},
+    {"1976", "DC"}, {"1989", "Marvel"}, {"2001", "Marvel"}, {"1982", "DC"}, {"1975", "Marvel"}
+};
+
