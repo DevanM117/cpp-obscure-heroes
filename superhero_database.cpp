e9e28e391b358b50a848413fa7e21e7e8fe46eaa
DevanM117 <devanmarion8@gmail.com>
2025-04-21 01:57:12 +0000

    Added starting blocks

--- superhero_database.cpp
+++ superhero_database.cpp
@@ -1,1 +1,14 @@
+//************************************************************************
+//Author.....: Devan Marion
+//Assignment: Banking System
+//Despcription: This program provides a simple banking system where users
+//              can deposit, withdraw, check their balance, and exit. 
+//************************************************************************ 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
+using namespace std;
+
+const int SIZE = 10;
